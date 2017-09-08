8e257812e5bccb65fc986dd0749fbdc06f61babc
Reghuram Vasanthakumari <rvasanthakumari@vmware.com>
2017-09-08 11:09:26 +0530

    initial place commit with barely no data

---
+++ vertx-rxjava-ci-rca/ci-endpoint-configurator/src/main/java/ai/devops/reactive/launcher/EndPointConfiguratorVerticle.java
@@ -0,0 +1,12 @@
+package ai.devops.reactive.launcher;
+
+import io.vertx.core.AbstractVerticle;
+
+public class EndPointConfiguratorVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
